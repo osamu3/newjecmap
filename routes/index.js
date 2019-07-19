6552da1a2b9aad900d2ee0dcffa32d8702f40105
osamu3 <osamu@quwans.jp>
2019-07-19 10:20:06 +0900

    express スケルトン作成OK

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'New Jec Map 福知山版' });
 });
 
 module.exports = router;
